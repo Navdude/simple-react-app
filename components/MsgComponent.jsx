67e139d76c4465c3ed2417a5da800a9bad9a9af9
Navin Tyagi <ntyag4@sapient.com>
2017-11-28 13:15:25 +0530

    Simple ReAct Example using components

---
+++ components/MsgComponent.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class Msg extends React.Component{
+ render(){
+     return(<div>
+         Simple React Program 
+     </div>)
+ }
+}
+
+export default Msg;
+ 
---
+++ main.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Msg from './components/MsgComponent.jsx';
+
+ReactDOM.render(<Msg />, document.getElementById('app'));
